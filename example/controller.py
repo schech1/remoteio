94c9864c6f352b6f7a9a12226e58dd868b72e661
mbSolar <maximilian.bock@t-online.de>
2024-10-03 18:00:38 +0200

    Add files via upload

--- example/controller.py
+++ example/controller.py
@@ -1,21 +1,36 @@
 from remoteio import RemoteServer
-from time import sleep
+import time
+import logging
 
-if __name__ == "__main__":
-    time = 2000
-    server_ip = "192.168.1.38"
-    server_port = 1234
+    
+try:
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(name="remoteio")
+    raspi1='raspy5' 
+    # erstellt virtuelle Pins auf 2 verschiedenen Raspberry Pis
+    remote_pi=None
+    remote_pi=RemoteServer(raspi1)
+    # 'g ': BCM -B e z e i c h n u n g ( G P I O 2 1 )
+    remote_pin=remote_pi.pin(21,'g')
+    remote_pin1=remote_pi.pin(20,'g')
+    remote_pin2=remote_pi.pin(16,'g')
 
-    # Create instance of remote Raspberry Pi
-    remote_server = RemoteServer(server_ip, server_port)
-    remote_pin = remote_server.pin(8, 'b')
 
-    # Demo features
-    remote_pin.on(time_ms=time) # Time until switch off
-    sleep(time/1000)
-    remote_pin.blink()
-    sleep(1)
-    remote_pin.pulse()
-    sleep(1)
-    remote_pin.off()
-    remote_server.close()
+    
+
+    remote_pin.on(time_ms=10000)
+    remote_pin1.pulse(time_ms=10000)
+    remote_pin2.blink(time_ms=10000)
+    remote_pi.execute()
+    time.sleep(15.0)
+
+    
+    remote_pin.on(time_ms=20000)
+    remote_pi.execute()
+    while True:
+        pass
+except Exception as e:
+    logger.info(str(e))
+finally:
+    if remote_pi:
+        remote_pi.close()
