b51bcf2485cd26a2a0604cfc9ddadbb551a5a1d1
mbSolar <maximilian.bock@t-online.de>
2024-10-03 17:59:45 +0200

    Add files via upload

--- remoteio/remoteio_client.py
+++ remoteio/remoteio_client.py
@@ -1,17 +1,86 @@
-import socket
 
-class RemoteServer:
-    def __init__(self, server_ip, server_port):
+import socket
+PORT=8509                                                                               
+pin_map_gb={
+    0:27,
+    1:28,
+    2:3,
+    3:5,
+    4:7,
+    5:29,
+    6:31,
+    7:26,
+    8:24,
+    9:21,
+    10:19,
+    11:23,
+    12:32,
+    13:33,
+    14:8,
+    15:10,
+    16:36,
+    17:11,
+    18:12,
+    19:35,
+    20:38,
+    21:40,
+    22:15,
+    23:16,
+    24:18,
+    25:22,
+    26:37,
+    27:13
+}
+
+class RemoteServer:  
+
+    def __init__(self, server_ip, server_port=PORT):                                          
         self.server_ip = server_ip
         self.server_port = server_port
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client_socket.connect((self.server_ip, self.server_port))
+        self._pin_list=[]
+
+    def map_g_to_b(self,pin_number,numbering):
+        if numbering=='g':
+            return pin_map_gb[pin_number], 'b'
+            
+        if numbering == 'b':
+            if pin_number in pin_map_gb.values():
+                return pin_number,numbering
+            else:
+                raise ValueError('((b), ' + str(pin_number) + ') false')
+        
 
     def pin(self, pin_number, numbering='b'):
-        return RemotePin(self.client_socket, pin_number, numbering)
+        pin_number_new,numbering_new = self.map_g_to_b(pin_number,numbering)
+        for p in self._pin_list:
+            if p.pin_number==pin_number_new and p.numbering==numbering_new:
+                raise ValueError('(('+ str(numbering) + '), ' + str(pin_number) + ') already created as ' + 
+                                 '((' + str(numbering_new) + '), ' + str(pin_number_new)+')')
+            
+        p = RemotePin(self.client_socket, pin_number_new, numbering_new)
+        self._pin_list.append(p)
+        return p
 
     def close(self):
         self.client_socket.close()
+
+    def execute(self):
+        # synchronisation with server
+        self.client_socket.sendall('?'.encode())
+        x=self.client_socket.recv(1024).decode().strip()
+        if not x:
+            raise RuntimeError('Server disconnected')
+        if x!='!':
+            raise RuntimeError('synchronization with server fails')
+        data=''
+        for p in self._pin_list:
+            if p.aktiv==True:
+                p.aktiv=False
+                data += f"{p.numbering} {p.pin_number} {p.state} {p.time_ms} "
+        self.client_socket.sendall(data.encode())
+        
 
 class RemotePin:
     
@@ -19,49 +88,66 @@ class RemotePin:
         self.client_socket = client_socket
         self.pin_number = pin_number
         self.numbering = numbering
+        self.state = 'off'
         self.time_ms = 0
-
-    def __create_command(self, command:str, time_ms:int=0):
-        cmd = f"{command} {time_ms}"
-        return cmd
-    
-    def __send_command(self, command):
-        command = f"{self.numbering} {self.pin_number} {command}"
-        self.client_socket.sendall(command.encode())
+        self.aktiv=False
     
     def on(self, time_ms:int=0):
-        cmd = self.__create_command("on", time_ms)
-        self.__send_command(cmd)
-        return self
+        self.state='on'
+        self.time_ms=time_ms
+        self.aktiv=True
     
-    def blink(self):
-        cmd = self.__create_command("blink")
-        self.__send_command(cmd)
-        return self
-    
-    def pulse(self):
-        cmd = self.__create_command("pulse")
-        self.__send_command(cmd)
-        return self
+    def blink(self, time_ms:int=0):
+        self.state='blink'
+        self.time_ms=time_ms
+        self.aktiv=True
+       
+    def pulse(self, time_ms:int=0):
+        self.state='pulse'
+        self.time_ms=time_ms
+        self.aktiv=True
 
     def off(self):
-        cmd = self.__create_command("off")
-        self.__send_command(cmd)
-        return self
-
+        self.state='off'
+        self.time_ms=0
+        self.aktiv=True
 
 
 
 # Example usage:
 if __name__ == "__main__":
-    server_ip = "192.168.0.90"
-    server_port = 8509
+    import logging
+    import time
+    
+    try:
+        logging.basicConfig(level=logging.INFO)
+        logger = logging.getLogger(name="remoteio")
+        raspi1='raspy5' 
+        # erstellt virtuelle Pins auf 2 verschiedenen Raspberry Pis
+        remote_pi=None
+        remote_pi=RemoteServer(raspi1)
+        # 'g ': BCM -B e z e i c h n u n g ( G P I O 2 1 )
+        remote_pin=remote_pi.pin(21,'g')
+        remote_pin1=remote_pi.pin(20,'g')
+        remote_pin2=remote_pi.pin(16,'g')
 
-    remote_server = RemoteServer(server_ip, server_port)
-    remote_pin = remote_server.pin(8, 'b')
-    remote_pin.on(time_ms=2000) # Time in ms until switch off
-    remote_pin.blink()
-    remote_pin.on()  # Turn on the pin with the applied settings
-    remote_pin.off()  # Turn off the pin
-    remote_server.close()
- 
+
+        
+
+        remote_pin.on(time_ms=10000)
+        remote_pin1.pulse(time_ms=10000)
+        remote_pin2.blink(time_ms=10000)
+        remote_pi.execute()
+        time.sleep(15.0)
+        # Sonderfunktionen
+        
+        
+        remote_pin.on(time_ms=20000)
+        remote_pi.execute()
+        #while True:
+        #   pass
+    except Exception as e:
+        logger.info(str(e))
+    finally:
+        if remote_pi:
+            remote_pi.close()
--- remoteio/remoteio_server.py
+++ remoteio/remoteio_server.py
@@ -4,29 +4,45 @@ import threading
 import time
 import logging
 
+from remoteio import PORT      
+
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(name="remoteio")
 
 PIN_NUMBERINGS=['b', 'g'] # b: Board numbering, g = GPIO numbering
-PORT = 8509
+
 
 # Dictionary to store LED instances for each pin number
 led_dict = {}
-
+#list of leds in process
+led_list=[]
 # Define a dictionary to map status to LED actions
 process_command = {
-    "on": lambda led, time_ms: led.on() if int(time_ms) <= 0 else handle_timer(led, time_ms),
-    "off": lambda led, time_ms: led.off(),
-    "blink": lambda led, time_ms: led.blink(),
-    "pulse": lambda led, time_ms: led.pulse(),
+    "on": lambda led, time_ms:     threading.Thread(target=handle_timer, args=(led,'on',time_ms)).start(),
+    "blink": lambda led, time_ms:  threading.Thread(target=handle_timer, args=(led,'blink',time_ms)).start(),
+    "pulse": lambda led, time_ms:  threading.Thread(target=handle_timer, args=(led,'pulse',time_ms)).start(),
+    
 }
 
-def handle_timer(led, time_ms):
+def handle_timer(led, state, time_ms):
     time_ms = float(time_ms) / 1000.0
-    led.on()
-    time.sleep(time_ms)
-    led.off()
+ 
+    match state:
+        case 'on':
+            led.on()
+        case 'blink':
+            led.blink()
+        case 'pulse':
+            led.pulse()
+
+    if time_ms > 0.:
+        time.sleep(time_ms)
+        led.off()
+    led_list.remove(led)
+    
+    
+
 
 
 # Create gpiozero LED objects based on numbering system
@@ -38,37 +54,73 @@ def create_led(numbering, pin_number):
     else:
         logger.error(f"Invalid numbering system. Use {PIN_NUMBERINGS}")
 
+
+# preparation of a map to a list of lists of 4 elements                                         
+def map_to_list(m:map)->list:                                                                   
+    from math import fmod
+    try:
+        m_list=list(m)
+        if fmod(len(m_list),4) != 0:
+            raise ValueError("length of map not a multiple of 4")
+        
+        n_list=[]
+        for i in range(0,len(m_list),4):
+            n_list.append([m_list[i],m_list[i+1],m_list[i+2],m_list[i+3]])
+        ret=n_list
+    except Exception as e:
+        logger.error(f"Error: {e}")
+        ret=None
+    finally:
+        return ret
+########################################################################################################################
+#     
 # Handle client requests
 def handle_client(conn):
     try:
         while True:
+            #protection against to fast proceeding of the client
+            while len(led_list)>0:
+                pass
+            # client asks for sending of next task (execute-function)
             data = conn.recv(1024).decode().strip()
-            logger.info(data)
+            if not data:
+                break
+            if data=='?':
+                conn.send('!'.encode())
+            else:
+                raise RuntimeError('synchronisation with client fails')
+            
+            data = conn.recv(1024).decode().strip()
             if not data:
                 break
             try:
                 logger.debug(data)
-                numbering, pin, command, time_ms = map(str.strip, data.split())
+                liste = map_to_list(map(str.strip, data.split()))
+                if liste == None:
+                    raise ValueError("transformation of map to list did not succeed")
+                for [numbering, pin, command, time_ms] in liste:
+                    # Create or retrieve LED instance for the specified pin number
+                    if pin not in led_dict:
+                        led_dict[pin] = create_led(numbering, pin)
+                    led = led_dict[pin]
+                    logger.info(f"Pin: {pin}({numbering}), State: {command}, Time: {time_ms}")
 
-                # Create or retrieve LED instance for the specified pin number
-                if pin not in led_dict:
-                    led_dict[pin] = create_led(numbering, pin)
-                led = led_dict[pin]
+                    # Check if status is valid
+                    if command not in process_command:
+                        logger.error("Invalid command")
+                        continue
 
-                logger.info(f"Pin: {pin}({numbering}), State: {command}, Time: {time_ms}")
-
-                # Check if status is valid
-                if command not in process_command:
-                    logger.error("Invalid command")
-                    continue
-
-                # Execute gpio action 
-                process_command[command](led, time_ms)
+                    # Execute gpio action 
+                    led_list.append(led)
+                    process_command[command](led, time_ms)
 
             except ValueError as e:
                 logger.error(e)
+                break                       
             except Exception as e:
                 logger.error(f"Error: {e}")
+                break                      
+
     finally:
         # Cleanup actions on disconnect
         logger.info(f"Disconnected from client")
@@ -81,17 +133,19 @@ def handle_client(conn):
         conn.close()
 
 
-def run_server():
+def run_server(port=PORT):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('0.0.0.0', PORT))
+    server_socket.bind(('', port))          
     server_socket.listen(5)
-    logger.info(f"remoteio listening on port {PORT}...")
+    logger.info(f"remoteio listening on port {port}...")
 
     while True:
         conn, addr = server_socket.accept()
         logger.info(f"Connection from {addr}")
-        client_handler = threading.Thread(target=handle_client, args=(conn,))
+        client_handler=threading.Thread(target=handle_client, args=(conn,))
         client_handler.start()
+    
+    
 
 if __name__ == "__main__":
     run_server()
