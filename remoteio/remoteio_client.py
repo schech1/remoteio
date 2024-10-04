ac865c52a7b2c98e541be7400ce3f7eac2297d97
mbSolar <maximilian.bock@t-online.de>
2024-10-05 01:28:29 +0200

    Add files via upload

--- remoteio/remoteio_client.py
+++ remoteio/remoteio_client.py
@@ -1,16 +1,75 @@
-import socket
 
-class RemoteServer:
-    def __init__(self, server_ip, server_port):
+import socket
+import threading
+
+PORT=8509   
+
+pin_map_gb={
+    0:27,
+    1:28,
+    2:3,
+    3:5,
+    4:7,
+    5:29,
+    6:31,
+    7:26,
+    8:24,
+    9:21,
+    10:19,
+    11:23,
+    12:32,
+    13:33,
+    14:8,
+    15:10,
+    16:36,
+    17:11,
+    18:12,
+    19:35,
+    20:38,
+    21:40,
+    22:15,
+    23:16,
+    24:18,
+    25:22,
+    26:37,
+    27:13
+}
+
+
+
+class RemoteServer:  
+
+    def __init__(self, server_ip, server_port=PORT):   
+        global my_server                                       
         self.server_ip = server_ip
         self.server_port = server_port
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client_socket.connect((self.server_ip, self.server_port))
+        self._pin_list=[]
+
+    def map_g_to_b(self,pin_number,numbering):
+        if numbering=='g':
+            return pin_map_gb[pin_number], 'b'
+            
+        if numbering == 'b':
+            if pin_number in pin_map_gb.values():
+                return pin_number,numbering
+            else:
+                raise ValueError('((b), ' + str(pin_number) + ') false')
+        
 
     def pin(self, pin_number, numbering='b'):
-        return RemotePin(self.client_socket, pin_number, numbering)
+        pin_number_new,numbering_new = self.map_g_to_b(pin_number,numbering)
+        for p in self._pin_list:
+            if p.pin_number==pin_number_new and p.numbering==numbering_new:
+                raise ValueError('(('+ str(numbering) + '), ' + str(pin_number) + ') already created as ' + 
+                                 '((' + str(numbering_new) + '), ' + str(pin_number_new)+')')
+            
+        p = RemotePin(self.client_socket, pin_number_new, numbering_new)
+        self._pin_list.append(p)
+        return p
 
-    def close(self):
+    def close(self):       
         self.client_socket.close()
 
 class RemotePin:
@@ -19,49 +78,77 @@ class RemotePin:
         self.client_socket = client_socket
         self.pin_number = pin_number
         self.numbering = numbering
+        self.state='off'
         self.time_ms = 0
+    
+    def execute(self):
+        # data transfer
+        data=''
+        data += f"{self.numbering} {self.pin_number} {self.state} {self.time_ms} "
+        self.client_socket.sendall(data.encode())
 
-    def __create_command(self, command:str, time_ms:int=0):
-        cmd = f"{command} {time_ms}"
-        return cmd
-    
-    def __send_command(self, command):
-        command = f"{self.numbering} {self.pin_number} {command}"
-        self.client_socket.sendall(command.encode())
-    
+     
     def on(self, time_ms:int=0):
-        cmd = self.__create_command("on", time_ms)
-        self.__send_command(cmd)
-        return self
+        self.state='on'
+        self.time_ms=time_ms
+        self.execute()
     
-    def blink(self):
-        cmd = self.__create_command("blink")
-        self.__send_command(cmd)
-        return self
-    
-    def pulse(self):
-        cmd = self.__create_command("pulse")
-        self.__send_command(cmd)
-        return self
+    def blink(self, time_ms:int=0):
+        self.state='blink'
+        self.time_ms=time_ms
+        self.execute()
+       
+    def pulse(self, time_ms:int=0):
+        self.state='pulse'
+        self.time_ms=time_ms
+        self.execute() 
 
     def off(self):
-        cmd = self.__create_command("off")
-        self.__send_command(cmd)
-        return self
-
-
+        self.state='off'
+        self.time_ms=0
+        self.execute()
+ 
 
 
 # Example usage:
 if __name__ == "__main__":
-    server_ip = "192.168.0.90"
-    server_port = 8509
+    import logging
+    import time
+    
+    try:  
+        logging.basicConfig(level=logging.INFO)
+        logger = logging.getLogger(name="remoteio")
+        raspi1='raspy5' 
+        # erstellt virtuelle Pins auf 2 verschiedenen Raspberry Pis
+        remote_pi=None
+        remote_pi=RemoteServer(raspi1)
 
-    remote_server = RemoteServer(server_ip, server_port)
-    remote_pin = remote_server.pin(8, 'b')
-    remote_pin.on(time_ms=2000) # Time in ms until switch off
-    remote_pin.blink()
-    remote_pin.on()  # Turn on the pin with the applied settings
-    remote_pin.off()  # Turn off the pin
-    remote_server.close()
- 
+
+        # 'g ': BCM -B e z e i c h n u n g ( G P I O 2 1 )
+        remote_pin=remote_pi.pin(21,'g')
+        remote_pin1=remote_pi.pin(20,'g')
+        remote_pin2=remote_pi.pin(16,'g')
+
+
+        
+        while True:
+            remote_pin.on()
+            remote_pin1.pulse()
+            remote_pin2.blink()
+            time.sleep(20.0)
+        #remote_pin1.pulse()
+        #remote_pin2.blink()
+
+            
+            
+            #remote_pin1.off()
+            #remote_pin2.off()
+
+       
+        while True:
+            pass
+    except Exception as e:
+        logger.info(str(e))
+    finally:
+        if remote_pi:
+            remote_pi.close()
