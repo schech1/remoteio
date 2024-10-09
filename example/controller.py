accecbe3e2b3f7dfd75d5fb66c2c69a728096464
mbSolar <maximilian.bock@t-online.de>
2024-10-09 13:55:24 +0200

    Update controller.py

--- example/controller.py
+++ example/controller.py
@@ -2,7 +2,7 @@ from remoteio import RemoteServer
 from time import sleep
 
 if __name__ == "__main__":
-    time = 2000
+    time = 4000
     server_ip = "192.168.1.38"
     server_port = 1234
 
@@ -14,12 +14,12 @@ if __name__ == "__main__":
     remote_pin.on(time_ms=time) # Time until switch off
     sleep(time/1000)
     remote_pin.blink()
-    sleep(1)
+    sleep(4)
     remote_pin.pulse()
-    sleep(1)
+    sleep(4)
     remote_pin.blink(arg1=0.1,arg2=0.2)
-    sleep(1)
+    sleep(4)
     remote_pin.value(arg1=0.5)
-    sleep(1)
+    sleep(4)
     remote_pin.off()
     remote_server.close()
